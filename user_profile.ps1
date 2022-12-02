a1a272680d121028bfc614b74d89e3d76722a5a1
Sako <78461130+Sakooooo@users.noreply.github.com>
2022-12-02 18:24:36 +0400

    theme change

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,5 +1,5 @@
 # the fricken shell
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/lambda.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/honukai.omp.json" | Invoke-Expression
 
 # goofy ass modules
 Import-Module Terminal-Icons
@@ -8,7 +8,7 @@ Import-Module Terminal-Icons
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 
-# alias
+# alias and shit
 Set-Alias make mingw32-make
 
 # god i hate windows
